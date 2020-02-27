525f11a69da82338537cf6d4b2c9bc57689b7176
rajneesh <rajneeshkumar5300@gmail.com>
2020-02-27 15:45:15 +0530

    web view

---
+++ app/src/main/java/com/example/webviewexample/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.webviewexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    ProgressBar progressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressBar = findViewById(R.id.progress);
+        webView = findViewById(R.id.webview);
+        webView.loadUrl("https://www.google.com");
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                progressBar.setProgress(newProgress);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+       if(webView.canGoBack()){
+            webView.goBack();
+       }else{
+           super.onBackPressed();
+       }
+    }
+
+}
